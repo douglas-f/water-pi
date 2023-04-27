3c0f2cdaec16d3f5aac4ec94dfa5527a112fd1b3
Douglas Francis <douglas@poshops.io>
2023-04-27 12:05:30 -0500

    added error handling

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask, render_template, jsonify
 import RPi.GPIO as GPIO
 import threading
-valve = 37
+
+valve_pin = 37
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(valve, GPIO.OUT)
+GPIO.setup(valve_pin, GPIO.OUT)
 
 app = Flask(__name__)
 
-
-
 def close_valve_after_delay(delay):
-    threading.Timer(delay, lambda: GPIO.output(valve, False)).start()
+    threading.Timer(delay, lambda: GPIO.output(valve_pin, False)).start()
 
 @app.route('/')
 def index():
@@ -20,17 +19,26 @@ def index():
 
 @app.route('/status')
 def status():
-    valve_status = GPIO.input(valve)
-    return jsonify({'status': valve_status})
+    try:
+        valve_status = GPIO.input(valve_pin)
+        return jsonify({'status': valve_status})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 @app.route('/toggle')
 def toggle():
-    current_status = GPIO.input(valve)
-    new_status = not current_status
-    GPIO.output(valve, new_status)
-    if new_status:  # If the valve is opened
-        close_valve_after_delay(10)  # Close the valve after 10 seconds
-    return '', 204
+    try:
+        current_status = GPIO.input(valve_pin)
+        new_status = not current_status
+        GPIO.output(valve_pin, new_status)
+        if new_status:  # If the valve is opened
+            close_valve_after_delay(10)  # Close the valve after 10 seconds
+        return '', 204
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='10.250.1.205', port=5000)
+    try:
+        app.run(host='10.250.1.205', port=5000)
+    except Exception as e:
+        print('Error:', e)
